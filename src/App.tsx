2e1ebc0c0a87e922d00c205ac46f0c11c626269d
Jesse Navarro <jnavarro30@live.com>
2022-06-09 14:58:29 -0700

    install react-router and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <Router>
+      Hello
+    </Router>
   );
 }
 
