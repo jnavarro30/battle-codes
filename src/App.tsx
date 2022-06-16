302b1f93a11f9fa8e4b4e0eefeeaf08f8fecf06d
Jesse Navarro <jnavarro30@live.com>
2022-06-16 12:33:12 -0700

    setup react, typescript, and mantine

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  console.warn('Warning yo')
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
